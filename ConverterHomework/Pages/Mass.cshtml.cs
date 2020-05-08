b61803956b590157cd7a2e776bd390712ea304bf
Dorkwoodgames <dorkwoodgames@gmail.com>
2020-05-08 10:44:52 -0700

    Add project files.

---
+++ ConverterHomework/Pages/Mass.cshtml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ConverterHomework
+{
+    public class MassModel : PageModel
+    {
+        [BindProperty]
+        public int massType { get; set; }
+        [BindProperty]
+        public float value { get; set; }
+        [BindProperty]
+        public string conversion { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+        public void OnPost()
+        {
+
+            conversion = coversion(massType, value).ToString();
+
+        }
+
+        private float coversion(int conversion, float value ) 
+        {
+            switch (conversion)
+            {
+                case 0:
+                    value = value * 16;
+                    break;
+                case 1:
+                    value = value / 16;
+                    break;
+                case 2:
+                    value = value / 454;
+                    break;
+                case 3:
+                    value = value / 453592;
+                    break;
+
+            }
+            return value;
+        }
+    }
+}
---
+++ ConverterHomework/Pages/Temperature.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ConverterHomework
+{
+    public class TemperatureModel : PageModel
+    {
+        [BindProperty]
+        public int tempType { get; set; }
+        [BindProperty]
+        public float value { get; set; }
+        [BindProperty]
+        public string conversion { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+        public void OnPost()
+        {
+            //basic function need to add more 
+            conversion = coversion(tempType, value).ToString();
+
+        }
+
+        private float coversion(int conversion, float value)
+        {
+            switch (conversion)
+            {
+                case 0:
+                    value = value - 32;
+                    value = value * 5/9;
+                    break;
+                case 1:
+                    value = value * 9/5 + 32;
+                    break;
+
+            }
+            return value;
+        }
+    }
+}
